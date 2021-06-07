4736b95dde2de9f2548355f7081fee36ef8d679e
shaw <shawwian@gmail.com>
2021-06-06 23:01:16 -0400

    start ch4 problems

---
+++ ch4/problems.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+
+/* 4.45
+A. No it does not, it decrements rsp before pushing it on the stack.
+
+ex: 
+rsp = 100
+subq $8, %rsp         rsp = 92
+movq %rsp, (%rsp)     puts 92 on the stack
+
+B. 
+movq %rsp, %rdx
+subq $8, %rsp         
+movq %rdx, (%rsp)    
+*/
+
+
+/* 4.46 
+A. No it does not, it moves the value on the stack into rsp and then increments it.
+
+ex:
+rsp = 100
+top of stack = 69
+
+movq (%rsp), %rsp       rsp = 69
+addq $8, %rsp           rsp = 77
+
+B. 
+movq (%rsp), %rdx
+addq $8, %rsp
+movq %rdx, %rsp
+*/
+
+
+/* 4.47 */
+void bubble_a(long *data, long count) {
+    long i, last;
+    for (last = count-1; last > 0; last--) {
+        for (i = 0; i < last; i++) {
+            if (data[i+1] < data[i]) {
+                long t = data[i+1];
+                data[i+1] = data[i];
+                data[i] = t;
+            }
+        }
+    }
+}
+
+/* A. */
+void bubble_p(long *data, long count) {
+    long i, last;
+    for (last = count-1; last > 0; last--) {
+        for (i = 0; i < last; i++) {
+            long *curr = data+i;
+            long *next = data+i+1;
+            if (*next < *curr) {
+                long t = *next;
+                *next = *curr;
+                *curr = t;
+            }
+        }
+    }
+}
+
+void print_array(long *arr, long count) {
+    for (int i=0; i<count; i++)
+        printf("%ld ", arr[i]);
+    printf("\n");
+}
+
+
+/* 
+B. 
+Y86-64 Program written elsewhere, check sim/y86-code
+
+*/
+
+
+
+
+
+
+
+int main() {
+    long nums[9] = {6,2,3,5,9,1,4,8,7};
+    long count = 9;
+
+    printf("Before:\n");
+    print_array(nums, count);
+
+    bubble_a(nums, count);
+
+    printf("After:\n");
+    print_array(nums, count);
+
+    return 0;
+}
