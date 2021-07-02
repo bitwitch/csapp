899cb6dd892d1ce9b9e1e01311eb1de0121f457c
bitwitch <b1tw1tch@pm.me>
2021-07-02 01:23:29 -0400

    optimize matrix transpose

--- ch6/problems.c
+++ ch6/problems.c
@@ -3,84 +3,31 @@
 #include <stdlib.h>
 
 void transpose(int *dst, int *src, long dim) {
-    long i, j;
+    long i=0, j, k, l;
     int stride = 8;
-    for (i=0; i<dim-7; i+=stride) {
-        for (j=0; j<dim-7; j+=stride) {
 
-            dst[(j+0)*dim+i] = src[i*dim+j+0];
-            dst[(j+1)*dim+i] = src[i*dim+j+1];
-            dst[(j+2)*dim+i] = src[i*dim+j+2];
-            dst[(j+3)*dim+i] = src[i*dim+j+3];
-            dst[(j+4)*dim+i] = src[i*dim+j+4];
-            dst[(j+5)*dim+i] = src[i*dim+j+5];
-            dst[(j+6)*dim+i] = src[i*dim+j+6];
-            dst[(j+7)*dim+i] = src[i*dim+j+7];
+    for (i=0; i<dim-(stride-1); i+=stride) {
+        for (j=0; j<dim-(stride-1); j+=stride) {
 
-            dst[(j+0)*dim+i+1] = src[(i+1)*dim+j+0];
-            dst[(j+1)*dim+i+1] = src[(i+1)*dim+j+1];
-            dst[(j+2)*dim+i+1] = src[(i+1)*dim+j+2];
-            dst[(j+3)*dim+i+1] = src[(i+1)*dim+j+3];
-            dst[(j+4)*dim+i+1] = src[(i+1)*dim+j+4];
-            dst[(j+5)*dim+i+1] = src[(i+1)*dim+j+5];
-            dst[(j+6)*dim+i+1] = src[(i+1)*dim+j+6];
-            dst[(j+7)*dim+i+1] = src[(i+1)*dim+j+7];
+            for (l=0; l<stride; l++) {
+                for (k=0; k<stride; k++) {
+                    dst[(j+k)*dim+i+l] = src[(i+l)*dim+j+k];
+                }
+            }
 
-            dst[(j+0)*dim+i+2] = src[(i+2)*dim+j+0];
-            dst[(j+1)*dim+i+2] = src[(i+2)*dim+j+1];
-            dst[(j+2)*dim+i+2] = src[(i+2)*dim+j+2];
-            dst[(j+3)*dim+i+2] = src[(i+2)*dim+j+3];
-            dst[(j+4)*dim+i+2] = src[(i+2)*dim+j+4];
-            dst[(j+5)*dim+i+2] = src[(i+2)*dim+j+5];
-            dst[(j+6)*dim+i+2] = src[(i+2)*dim+j+6];
-            dst[(j+7)*dim+i+2] = src[(i+2)*dim+j+7];
-
-            dst[(j+0)*dim+i+3] = src[(i+3)*dim+j+0];
-            dst[(j+1)*dim+i+3] = src[(i+3)*dim+j+1];
-            dst[(j+2)*dim+i+3] = src[(i+3)*dim+j+2];
-            dst[(j+3)*dim+i+3] = src[(i+3)*dim+j+3];
-            dst[(j+4)*dim+i+3] = src[(i+3)*dim+j+4];
-            dst[(j+5)*dim+i+3] = src[(i+3)*dim+j+5];
-            dst[(j+6)*dim+i+3] = src[(i+3)*dim+j+6];
-            dst[(j+7)*dim+i+3] = src[(i+3)*dim+j+7];
-
-            dst[(j+0)*dim+i+4] = src[(i+4)*dim+j+0];
-            dst[(j+1)*dim+i+4] = src[(i+4)*dim+j+1];
-            dst[(j+2)*dim+i+4] = src[(i+4)*dim+j+2];
-            dst[(j+3)*dim+i+4] = src[(i+4)*dim+j+3];
-            dst[(j+4)*dim+i+4] = src[(i+4)*dim+j+4];
-            dst[(j+5)*dim+i+4] = src[(i+4)*dim+j+5];
-            dst[(j+6)*dim+i+4] = src[(i+4)*dim+j+6];
-            dst[(j+7)*dim+i+4] = src[(i+4)*dim+j+7];
-
-            dst[(j+0)*dim+i+5] = src[(i+5)*dim+j+0];
-            dst[(j+1)*dim+i+5] = src[(i+5)*dim+j+1];
-            dst[(j+2)*dim+i+5] = src[(i+5)*dim+j+2];
-            dst[(j+3)*dim+i+5] = src[(i+5)*dim+j+3];
-            dst[(j+4)*dim+i+5] = src[(i+5)*dim+j+4];
-            dst[(j+5)*dim+i+5] = src[(i+5)*dim+j+5];
-            dst[(j+6)*dim+i+5] = src[(i+5)*dim+j+6];
-            dst[(j+7)*dim+i+5] = src[(i+5)*dim+j+7];
-
-            dst[(j+0)*dim+i+6] = src[(i+6)*dim+j+0];
-            dst[(j+1)*dim+i+6] = src[(i+6)*dim+j+1];
-            dst[(j+2)*dim+i+6] = src[(i+6)*dim+j+2];
-            dst[(j+3)*dim+i+6] = src[(i+6)*dim+j+3];
-            dst[(j+4)*dim+i+6] = src[(i+6)*dim+j+4];
-            dst[(j+5)*dim+i+6] = src[(i+6)*dim+j+5];
-            dst[(j+6)*dim+i+6] = src[(i+6)*dim+j+6];
-            dst[(j+7)*dim+i+6] = src[(i+6)*dim+j+7];
-
-            dst[(j+0)*dim+i+7] = src[(i+7)*dim+j+0];
-            dst[(j+1)*dim+i+7] = src[(i+7)*dim+j+1];
-            dst[(j+2)*dim+i+7] = src[(i+7)*dim+j+2];
-            dst[(j+3)*dim+i+7] = src[(i+7)*dim+j+3];
-            dst[(j+4)*dim+i+7] = src[(i+7)*dim+j+4];
-            dst[(j+5)*dim+i+7] = src[(i+7)*dim+j+5];
-            dst[(j+6)*dim+i+7] = src[(i+7)*dim+j+6];
-            dst[(j+7)*dim+i+7] = src[(i+7)*dim+j+7];
         }
     }
+
+    long remaining = j;
+
+    for (i=0; i<remaining; i++)
+        for (j=remaining; j<dim; j++)
+            dst[j*dim+i] = src[i*dim+j];
+
+    for (; i<dim; i++)
+        for (j=0; j<dim; j++)
+            dst[j*dim+i] = src[i*dim+j];
+
 }
 
 void print_matrix(int *m, long dim) {
@@ -102,9 +49,19 @@ void init_matrix(int *m, long dim) {
     }
 }
 
+int check_transpose(int *m1, int *m2, long dim) {
+    long i, j;
+    for (i=0; i<dim; i++)
+        for (j=0; j<dim; j++)
+            if (m1[j*dim+i] != m2[i*dim+j])
+                return 0;
+    return 1;
+}
 
 
-#define SIZE1 5
+
+
+#define SIZE1 10
 #define SIZE2 34080
 
 int main() {
@@ -121,6 +78,7 @@ int main() {
     printf("After:\n");
     print_matrix(m2, SIZE1);
 
+    assert(check_transpose(m1,m2,SIZE1));
 
     int *m3 = malloc((long)SIZE2 * (long)SIZE2 * sizeof(int));
     if (m3 == NULL) {
@@ -138,6 +96,8 @@ int main() {
     init_matrix(m3, SIZE2);
 
     transpose(m4, m3, SIZE2);
+
+    assert(check_transpose(m3,m4,SIZE2));
 
     free(m3);
     free(m4);
